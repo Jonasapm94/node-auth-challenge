3fd2dd69c2439133758ad7ff5048fa92d146228d
Jonas Ariel <jonas.ariel@codeminer42.com>
2025-04-03 13:35:48 -0300

    pages with users logged in will show the username and logout link

--- src/http/plugins/authPlugin.ts
+++ src/http/plugins/authPlugin.ts
@@ -1,16 +1,31 @@
 import { Authenticator } from '@fastify/passport';
-import { FastifyPluginAsync } from 'fastify';
+import { FastifyPluginAsync, FastifyPluginCallback, FastifyReply, FastifyRequest, preHandlerAsyncHookHandler } from 'fastify';
 import plugin from 'fastify-plugin';
 import { Strategy as LocalStrategy } from 'passport-local';
 import { UserModel } from '../../database/models/UserModel.js';
 import * as UsersController from '../controllers/usersController.js';
 import * as SessionsController from '../controllers/sessionsController.js';
+import 'fastify';
+
+// it was declared here instead of another file because otherwise nodemin crashes 
+// (it doesn't finds the type declaration in other file)
+declare module 'fastify' {
+  interface FastifyReply {
+    locals?: any;
+  }
+}
 
 export const authPlugin: FastifyPluginAsync = plugin(async (server, _) => {
   const authenticator = new Authenticator();
 
   server.register(authenticator.initialize());
   server.register(authenticator.secureSession());
+
+  server.addHook('preHandler', (request, reply, done) => {
+    reply.locals = reply.locals || {};
+    reply.locals.currentUser = request.user || null;
+    done()
+  })
 
   authenticator.use(
     'localStrategy',
