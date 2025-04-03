48d5cc518d46df729c34842270b51d12fef6dd07
Jonas Ariel <jonas.ariel@codeminer42.com>
2025-04-03 18:55:42 -0300

    All dealerships pages must be authenticated

---
+++ tests/dealerships/editDealership.test.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from "fastify";
+import { beforeAll, describe, expect, it } from "vitest";
+import { makeServer } from "../../src/http/server.js";
+
+describe("GET /dealerships/:id/edit", () => {
+    let server: FastifyInstance
+
+    beforeAll(async () => {
+        server = await makeServer();
+    })
+
+    describe("when the user who made the request is not logged in", () => {
+        it("redirects the request to login page", async () => {
+            const response = await server.inject({
+                method: 'GET',
+                url: '/dealerships/1/edit'
+            })
+
+            expect(response.statusCode).toBe(302)
+            expect(response.headers.location).toBe("/login")
+        })
+    })
+})
---
+++ tests/dealerships/listDealerships.test.ts
@@ -0,0 +1,22 @@
+import { FastifyInstance } from "fastify";
+import { beforeAll, describe, expect, it } from "vitest";
+import { makeServer } from "../../src/http/server.js";
+
+describe("GET /dealerships", () => {
+    let server: FastifyInstance
+
+    beforeAll(async () => {
+        server = await makeServer();
+    })
+
+    describe("when the user who made the request is not logged in", () => {
+        it("redirects the request to login page", async () => {
+            const response = await server.inject({
+                url: '/dealerships'
+            })
+
+            expect(response.statusCode).toBe(302)
+            expect(response.headers.location).toBe("/login")
+        })
+    })
+})
