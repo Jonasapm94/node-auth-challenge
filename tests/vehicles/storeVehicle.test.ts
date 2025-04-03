7fd5ac8a2e7ef2f42ebd9c677be8b33f3830e449
Jonas Ariel <jonas.ariel@codeminer42.com>
2025-04-03 19:00:41 -0300

    All vehicles pages, except for index (which must be opened), must be authenticated

---
+++ tests/vehicles/storeVehicle.test.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from "fastify";
+import { beforeAll, describe, expect, it } from "vitest";
+import { makeServer } from "../../src/http/server.js";
+
+describe("POST /vehicles", () => {
+    let server: FastifyInstance
+
+    beforeAll(async () => {
+        server = await makeServer();
+    })
+
+    describe("when the user who made the request is not logged in", () => {
+        it("redirects the request to login page", async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: '/vehicles'
+            })
+
+            expect(response.statusCode).toBe(302)
+            expect(response.headers.location).toBe("/login")
+        })
+    })
+})
