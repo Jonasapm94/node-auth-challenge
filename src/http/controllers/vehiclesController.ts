d205d84771e548a4a31cf8de93e3c62e9a4d691c
Jonas Ariel <jonas.ariel@codeminer42.com>
2025-04-03 14:19:47 -0300

    get the dealershipId from the user who made the request when creating new vehicle

--- src/http/controllers/vehiclesController.ts
+++ src/http/controllers/vehiclesController.ts
@@ -1,4 +1,5 @@
 import { handler } from '../../_lib/http/handler.js';
+import { UserModel } from '../../database/models/UserModel.js';
 import { VehicleModel } from '../../database/models/VehicleModel.js';
 import { authenticatedHandler } from '../auth.js';
 
@@ -15,16 +16,19 @@ const create = authenticatedHandler(async (request, reply) => {
 const store = authenticatedHandler<{
   Body: { name: string; brand: string; model: string; year: string; comments: string; dealershipId: number };
 }>(async (request, reply) => {
-  const { name, brand, model, year, comments, dealershipId } = request.body;
+  const { name, brand, model, year, comments } = request.body;
 
   try {
+    const dealershipId = Number((request.user as UserModel).dealershipId);
+    if (!dealershipId) throw new Error('Dealership was not found from the request user');
+
     await VehicleModel.query().insert({ name, brand, model, year, comments, dealershipId });
 
     return reply.redirect(`/vehicles`);
   } catch (error) {
     console.error(error);
     return reply.view('vehicles/create', {
-      vehicle: new VehicleModel().$set({ name, brand, model, year, comments, dealershipId }),
+      vehicle: new VehicleModel().$set({ name, brand, model, year, comments }),
     });
   }
 });
